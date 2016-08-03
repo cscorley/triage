e65dee723693f365359db81b98bd95c86b14b8ed
Christopher Corley <cscorley@ua.edu>
2016-08-03 18:30:11 -0400

    blah

--- optimizer.sh
+++ optimizer.sh
@@ -8,8 +8,6 @@ mkdir -p ${logs}
 
 function run {
     for i in $(seq 1 1); do
-#        find data/${1} -name 'LDA*' -exec rm {} \;
-#        find data/${1} -name '*.lda*' -exec rm {} \;
 #        find data/${1} -name "*ranks*.csv.gz" -exec rm {} \;
 
         experiments=(feature_location triage)
@@ -17,6 +15,14 @@ function run {
             log_dest="${logs}/${i}-${1}-${2}-${experiment}.log"
             echo "running ${experiment} ${@} ${i}"
             date >> ${log_dest}
+
+            # to perform a "fair" evaluation and make sure each of them start at
+            # the same exact random state (including inferences) we need to
+            # start fresh each time.
+            #
+            # in other words, lmao
+            find data/${1} -name 'LDA*' -exec rm {} \;
+            find data/${1} -name '*.lda*' -exec rm {} \;
 
             time cdi -v \
                 --model lda \
